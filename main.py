093f1d28f146949f396fd62c5c96e2b6f797e499
Ajay Reddy <ajayreddy.fsd@gmail.com>
2024-10-11 08:36:00 -0400

    complete

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ soup = BeautifulSoup(response.text, 'html.parser')
 links = soup.select('.titleline>a') #to get the tags associated with the news title and links
 subtexts = soup.select('.subtext') #to get the whole subtext tags, so we can filter out later ones that have score
 votes = []
-news = []
 
 
+#to fill the votes array
 #to filter out subtexts without and with score
 for subtext in subtexts:
     if subtext.find('span', class_ = 'score'):
@@ -20,6 +20,7 @@ for subtext in subtexts:
 
 #fetches news's with >= 100 votes
 def get_news(links, votes):
+    news = []
     for index, vote in enumerate(votes):
         vote = int(vote.text.split(' ')[0]) #cleaning
         if vote >= 100:
@@ -29,12 +30,12 @@ def get_news(links, votes):
                 'votes': vote
             }
             news.append(dict)
+    news.sort(key = lambda x:x['votes'], reverse=True) #sorting based on the no.of votes in desc order
+    return news
 
 
-get_news(links, votes)
-news.sort(key = lambda x:x['votes'], reverse=True) #sorting based on the no.of votes in desc order
 
-print(news)
+print(get_news(links, votes))
 
 
 
